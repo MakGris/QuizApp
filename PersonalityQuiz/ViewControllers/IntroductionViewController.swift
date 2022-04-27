7439c7f547f7ab66461eb162d18aea2fff156210
MakGris <maxgrishenko@gmail.com>
2022-04-27 20:31:41 +0700

    initial coomit

---
+++ PersonalityQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Maksim Grischenko on 23.04.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+}
+
---
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ResultViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Maksim Grischenko on 26.04.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    private var dogCount = 0
+    private var catCount = 0
+    private var turtleCount = 0
+    private var rabbitCount = 0
+    
+    var answers: [Answer] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countAnimalType()
+        updateResult()
+        
+        self.navigationItem.setHidesBackButton(true, animated: true)
+       
+    }
+}
+
+extension ResultViewController {
+    
+    private func countAnimalType () {
+        for answer in answers {
+            if answer.animal == .dog {
+                dogCount += 1
+            } else if answer.animal == .cat {
+                catCount += 1
+            } else if answer.animal == .rabbit {
+                rabbitCount += 1
+            } else {
+                turtleCount += 1
+            }
+        }
+    }
+    
+    private func updateResult () {
+        if dogCount > catCount && dogCount > turtleCount && dogCount > rabbitCount {
+            resultLabel.text = "Вы - \(Animal.dog.rawValue)"
+            descriptionLabel.text = Animal.dog.definition
+        } else if catCount > dogCount && catCount > turtleCount && catCount > rabbitCount {
+            resultLabel.text = "Вы - \(Animal.cat.rawValue)"
+            descriptionLabel.text = Animal.dog.definition
+        } else if turtleCount > dogCount && turtleCount > catCount && turtleCount > rabbitCount {
+            resultLabel.text = "Вы - \(Animal.turtle.rawValue)"
+            descriptionLabel.text = Animal.turtle.definition
+        } else {
+            resultLabel.text = "Вы - \(Animal.rabbit.rawValue)"
+            descriptionLabel.text = Animal.rabbit.definition
+        }
+    }
+}
+
