ae242bb2a87756fa43abe12708aac6f6fbd8f6c7
MakGris <maxgrishenko@gmail.com>
2022-04-28 19:11:27 +0700

    Solution in one string

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -27,32 +27,34 @@ class ResultViewController: UIViewController {
 extension ResultViewController {
         
     private func updateResult () {
-        var allAnimalsInAnswers: [Animal: Int] = [:]
-        let animals = answers.map { $0.animal }
-        
-        for animal in animals {
-            if let countAnimal = allAnimalsInAnswers[animal] {
-                allAnimalsInAnswers.updateValue(countAnimal + 1, forKey: animal)
-            } else {
-                allAnimalsInAnswers[animal] = 1
-            }
-        }
+//        var allAnimalsInAnswers: [Animal: Int] = [:]
+//        let animals = answers.map { $0.animal }
+//
+//        for animal in animals {
+//            if let countAnimal = allAnimalsInAnswers[animal] {
+//                allAnimalsInAnswers.updateValue(countAnimal + 1, forKey: animal)
+//            } else {
+//                allAnimalsInAnswers[animal] = 1
+//            }
+//        }
 //        Можно сократить цикл следующим образом:
 //        Присваиваем значение в словарь по ключу animal, если ключ со значением есть, то прибавляем 1, если ключа со значением нет, то присваиваем 0 и прибавляем 1.
 /*
           for animal in animals {
-         let allAnimalInAnswers[animal] = (allAnimalInAnswers[animal] ?? 0) + 1
+ let allAnimalInAnswers[animal] = (allAnimalInAnswers[animal] ?? 0) + 1
  }
  */
 
-        let sortedAllAnimalsInAnswers = allAnimalsInAnswers.sorted { $0.value > $1.value }
-        guard let mostPopularAnimal = sortedAllAnimalsInAnswers.first?.key else { return }
+//        let sortedAllAnimalsInAnswers = allAnimalsInAnswers.sorted { $0.value > $1.value }
+//        guard let mostPopularAnimal = sortedAllAnimalsInAnswers.first?.key else { return }
+//        Решение в одну строку, создаем словарь из массива из значений animal, сортируем по убыванию и берем ключ с самым большим значением
+        let mostPopularAnimal = Dictionary(grouping: answers) { $0.animal }.sorted { $0.value.count > $1.value.count } .first?.key
         updateUI(with: mostPopularAnimal)
     }
     
-    private func updateUI(with animal: Animal) {
-        resultLabel.text = "Вы - \(animal.rawValue)"
-        descriptionLabel.text = "\(animal.definition)"
+    private func updateUI(with animal: Animal?) {
+        resultLabel.text = "Вы - \(animal?.rawValue ?? "🐶")"
+        descriptionLabel.text = "\(animal?.definition ?? "")"
     }
 }
 
