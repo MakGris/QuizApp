fba6405280292dee674813aef887ebfb3fa71cf9
MakGris <maxgrishenko@gmail.com>
2022-04-28 18:38:18 +0700

    Change method (add dictionary and use map function)

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -12,54 +12,39 @@ class ResultViewController: UIViewController {
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     
-    var answers: [Answer] = []
-    
-    private var dogCount = 0
-    private var catCount = 0
-    private var turtleCount = 0
-    private var rabbitCount = 0
+    var answers: [Answer]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        countAnimalType()
+
         updateResult()
         
-        self.navigationItem.setHidesBackButton(true, animated: true)
+        navigationItem.hidesBackButton = true
        
     }
 }
 
 extension ResultViewController {
-    
-    private func countAnimalType () {
-        for answer in answers {
-            if answer.animal == .dog {
-                dogCount += 1
-            } else if answer.animal == .cat {
-                catCount += 1
-            } else if answer.animal == .rabbit {
-                rabbitCount += 1
+        
+    private func updateResult () {
+        var allAnimalsInAnswers: [Animal: Int] = [:]
+        let animals = answers.map { $0.animal }
+        
+        for animal in animals {
+            if let countAnimal = allAnimalsInAnswers[animal] {
+                allAnimalsInAnswers.updateValue(countAnimal + 1, forKey: animal)
             } else {
-                turtleCount += 1
+                allAnimalsInAnswers[animal] = 1
             }
         }
+        let sortedAllAnimalsInAnswers = allAnimalsInAnswers.sorted { $0.value > $1.value }
+        guard let mostPopularAnimal = sortedAllAnimalsInAnswers.first?.key else { return }
+        updateUI(with: mostPopularAnimal)
     }
     
-    private func updateResult () {
-        if dogCount > catCount && dogCount > turtleCount && dogCount > rabbitCount {
-            resultLabel.text = "Вы - \(Animal.dog.rawValue)"
-            descriptionLabel.text = Animal.dog.definition
-        } else if catCount > dogCount && catCount > turtleCount && catCount > rabbitCount {
-            resultLabel.text = "Вы - \(Animal.cat.rawValue)"
-            descriptionLabel.text = Animal.dog.definition
-        } else if turtleCount > dogCount && turtleCount > catCount && turtleCount > rabbitCount {
-            resultLabel.text = "Вы - \(Animal.turtle.rawValue)"
-            descriptionLabel.text = Animal.turtle.definition
-        } else {
-            resultLabel.text = "Вы - \(Animal.rabbit.rawValue)"
-            descriptionLabel.text = Animal.rabbit.definition
-        }
+    private func updateUI(with animal: Animal) {
+        resultLabel.text = "Вы - \(animal.rawValue)"
+        descriptionLabel.text = "\(animal.definition)"
     }
 }
 
